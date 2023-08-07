66be30efb47edc361c4e0cc56b587dc72d3ed5bd
ShihabHT <shihabrahman122@gmail.com>
2023-08-07 16:15:11 +0600

    single num

---
+++ 70. SingleNumber/singleNumber.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+  Using XOR property this problem can easily be solved. As taking XOR of a numner
+  with itself results zero
+*/
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(int i : nums) res ^= i;
+        return res;
+    }
+};
+
+int main(){
+    vector<int> nums={-4,1,2,1,-4,2};
+    Solution sol;
+    cout<<sol.singleNumber(nums)<<endl;
+
+    return 0;
+}
+/*
+  136. Single Number
+Easy
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+*/
