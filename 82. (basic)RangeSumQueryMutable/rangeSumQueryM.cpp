6f8913599f3b1ab59996019cd66a53fe25d3650a
ShihabHT <shihabrahman122@gmail.com>
2023-08-10 19:56:18 +0600

    details

--- 82. (basic)RangeSumQueryMutable/rangeSumQueryM.cpp
+++ 82. (basic)RangeSumQueryMutable/rangeSumQueryM.cpp
@@ -58,37 +58,43 @@ int main(){
     return 0;
 }
 /*
-  303. Range Sum Query - Immutable
-Easy
+  307. Range Sum Query - Mutable
+Medium
 
-Given an integer array nums, handle multiple queries of the following type:
+Given an integer array nums, handle multiple queries of the following types:
 
-Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Update the value of an element in nums.
+Calculate the sum of the elements of nums between indices left and right inclusive where 
+left <= right.
 Implement the NumArray class:
 
 NumArray(int[] nums) Initializes the object with the integer array nums.
-int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and
-right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+void update(int index, int val) Updates the value of nums[index] to be val.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices
+left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
  
 
 Example 1:
+
 Input
-["NumArray", "sumRange", "sumRange", "sumRange"]
-[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+["NumArray", "sumRange", "update", "sumRange"]
+[[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
 Output
-[null, 1, -1, -3]
+[null, 9, null, 8]
 
 Explanation
-NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
-numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
-numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
-numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+NumArray numArray = new NumArray([1, 3, 5]);
+numArray.sumRange(0, 2); // return 1 + 3 + 5 = 9
+numArray.update(1, 2);   // nums = [1, 2, 5]
+numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
  
 
 Constraints:
 
-1 <= nums.length <= 104
--105 <= nums[i] <= 105
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+0 <= index < nums.length
+-100 <= val <= 100
 0 <= left <= right < nums.length
-At most 104 calls will be made to sumRange.
+At most 3 * 104 calls will be made to update and sumRange.
 */
